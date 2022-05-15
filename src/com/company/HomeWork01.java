ba29ae56f3f31c953da957f276e536e520223ca0
Samina-Razzaq <s.razzaq2012@gmail.com>
2022-05-14 21:15:26 -0700

    first code from HW

---
+++ src/com/company/HomeWork01.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class HomeWork01 {
+    public static void main(String[] args) {
+        //HW1:
+        //1.create a new IJ project
+        //2.add code in it that prints the numbers from 1 to 100
+        //3.send this code to github
+
+        for(int i=1; i<=100; i++){
+            System.out.print(i+" ");
+        }
+    }
+}
